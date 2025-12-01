7831ee9fda98b703fb1755768b9e8d5b0622fb7a
juanma <juanmaxp2@gmail.com>
2025-12-01 13:59:34 +0100

    test

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,7 @@ const nextConfig = {
   output: "export",
   basePath: "/juanma-webapp",
   assetPrefix: "/juanma-webapp/",
+  typeRoots: ["./types", "./node_modules/@types"]
 };
 
 export default nextConfig;
